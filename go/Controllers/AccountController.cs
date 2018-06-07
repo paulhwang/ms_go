38463175b78ce5f94c7a19e0b5e13fcb1b132ec9
paul.hwang <paul.hwang@getac.com.tw>
2018-06-07 14:22:01 +0800

    account

---
+++ go/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace go.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+
+        public ActionResult AccountRoot()
+        {
+            return View();
+        }
+
+        public ActionResult AccountSignIn()
+        {
+            return View();
+        }
+
+        public ActionResult AccountSignUp()
+        {
+            return View();
+        }
+    }
+}
