2f96b93facc1fb8c12c39103d14a181b01b0dede
paul.hwang <paul.hwang@getac.com.tw>
2018-06-07 10:13:10 +0800

    add Go controller and view

---
+++ go/Controllers/GoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace go.Controllers
+{
+    public class GoController : Controller
+    {
+        // GET: Go
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
